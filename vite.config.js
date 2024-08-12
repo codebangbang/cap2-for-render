2114ea754b61366f4d26cd28d9992ddc73c51d18
codebangbang <ryfair@gmail.com>
2024-08-12 09:50:12 -0400

    updated vite.config.js for render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills()],
+
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
