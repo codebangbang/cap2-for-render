b114f1366185e01e1737b181e626d841ffd97b4d
codebangbang <ryfair@gmail.com>
2024-08-22 11:20:42 -0400

    Installed lodash and am trying to provide a better search experience.

--- src/auth/SearchForm.jsx
+++ src/auth/SearchForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./SearchForm.css";
 
 // This is my SearchForm component. It displays a form for a user to search for an employee or a certain skill that they need help with. .
@@ -7,16 +7,29 @@ function SearchForm({ searchFor }) {
   // console.debug("SearchForm", "searchFor=", typeof searchFor);
     const [searchTerm, setSearchTerm] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    // console.debug("Submitting search with term:", searchTerm);
-    searchFor(searchTerm.trim());
-  }
+    const debouncedSearch = debounce((term) => {
+      searchFor(term.trim());
+    }, 300);
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   // console.debug("Submitting search with term:", searchTerm);
+  //   searchFor(searchTerm.trim());
+  // }
 
   function handleChange(e) {
     setSearchTerm(e.target.value);
   }
 
+  useEffect(() => {
+    if (searchTerm) {
+      debouncedSearch(searchTerm);
+    }
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [searchTerm, debouncedSearch]);
+  
   return (
     <div className="SearchForm mb-4">
     <form className="SearchForm form-inline mb-4" onSubmit={handleSubmit}>
